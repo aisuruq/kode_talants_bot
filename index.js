53b9416c4f66149a2e4bf76cfa265c19151ea346
aisuruqta <aisuruqta@gmail.com>
2024-10-30 23:06:02 +0200

    Глобальное изменение логики

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 require("dotenv").config();
-const tgApi = require(`node-telegram-bot-api`);
+const texts = require('./text');
+const telegramBot = require(`node-telegram-bot-api`);
 
-const bot = new tgApi(process.env.API_KEY_BOT, {
+const bot = new telegramBot(process.env.API_KEY_BOT, {
   polling: true,
 });
 
-bot.setMyCommands([{ command: "/start", description: "Запуск бота" }]);
 bot.setMyCommands(
   [
     { command: "/start", description: "Запуск бота" },
@@ -22,7 +22,8 @@ const langOptions = {
   }),
 };
 
-let selectedLanguage = "rus";
+let selectedLanguage;
+let userResponses = {}; // Для временного хранения данных пользователя
 
 bot.onText(/\/start/, (msg) => {
   bot.sendMessage(
@@ -32,46 +33,105 @@ bot.onText(/\/start/, (msg) => {
   );
 });
 
-let helpMessageRus = `
-Вот доступные команды:
-  /start - Запуск взаимодействия с ботом.
-  /help - Показать это сообщение помощи.
-Пожалуйста, дайте согласие на обработку данных для продолжения.`;
-
-let helpMessageEng = `
-Here are the available commands:
-  /start - Start the interaction with the bot.
-  /help - Show this help message.
-Please provide your consent for data processing to continue.`;
-
-bot.on("callback_query", (query) => {
+bot.on("callback_query", async (query) => {
   const chatId = query.message.chat.id;
-  const messageId = query.message.message_id;
-  const lang = query.data;
-  const MessageId = query.message.message_id;
+  const data = query.data;
 
   bot.answerCallbackQuery(query.id);
-  bot.deleteMessage(chatId, MessageId).catch((err) => console.log('Error deleting message:', err));
 
-  selectedLanguage = lang;
+  if (data === "rus" || data === "eng") {
+    selectedLanguage = data;
+    userResponses[chatId] = { language: selectedLanguage };
 
-  if (lang === "rus") {
-    bot.deleteMessage(chatId, messageId);
-    const ruScenario = require("./scenarios/rus");
-    ruScenario(bot, chatId);
-    const ruScenario = require("./scenarios/rus");
-    ruScenario(bot,chatId);
+    await bot.deleteMessage(chatId, query.message.message_id);
+    await bot.sendMessage(chatId, texts[selectedLanguage].welcome);
+    askSpeciality(chatId, selectedLanguage);
 
-  } else if (lang === "eng") {
-    bot.deleteMessage(chatId, messageId);
-    const enScenario = require("./scenarios/eng");
-    enScenario(bot, chatId);
-    const enScenario = require("./scenarios/eng");
-    enScenario(bot,chatId);
+  } else if  (data === `${selectedLanguage}_spec_yes`) {
+    await bot.sendMessage(chatId, texts[selectedLanguage].specYesResponse);
+    bot.deleteMessage(chatId, query.message.message_id);
+    requestConsent(chatId, selectedLanguage);
+  } else if (data === `${selectedLanguage}_spec_no`) {
+    await bot.sendMessage(chatId, texts[selectedLanguage].specNoResponse);
+    bot.deleteMessage(chatId, query.message.message_id);
+    requestConsent(chatId, selectedLanguage);
   }
+  
+  else if (data === `${selectedLanguage}_consent_agree`) {
+    await bot.sendMessage(chatId, texts[selectedLanguage].consentAgree);
+    bot.deleteMessage(chatId, query.message.message_id);
+    startRegistration(chatId, selectedLanguage);
 
-  bot.onText(/\/help/, (msg) => {
-    const helpMessage = selectedLanguage === "rus" ? helpMessageRus : helpMessageEng;
-    bot.sendMessage(msg.chat.id, helpMessage);
-  });
+  } else if (data === `${selectedLanguage}_consent_disagree`) {
+    bot.answerCallbackQuery(query.id);
+    const consentMessage = await bot.sendMessage(chatId, texts[selectedLanguage].consentDisagree);
+      setTimeout(async () => {
+      await bot.deleteMessage(chatId, consentMessage.message_id);
+      requestConsent(chatId, selectedLanguage);
+    }, 5000);
+
+    bot.deleteMessage(chatId, query.message.message_id);
+  }
 });
+
+const askSpeciality = (chatId, language) => {
+  const specOptions = {
+    reply_markup: JSON.stringify({
+      inline_keyboard: [
+        [{ text: language === "rus" ? "Да" : "Yes", callback_data: `${language}_spec_yes` }],
+        [{ text: language === "rus" ? "Нет" : "No", callback_data: `${language}_spec_no` }],
+      ],
+    }),
+  };
+
+  bot.sendMessage(chatId, texts[language].specialityQuestion, specOptions);
+};
+
+const requestConsent = (chatId, language) => {
+  const consentOptions = {
+    reply_markup: JSON.stringify({
+      inline_keyboard: [
+        [{ text: language === "rus" ? "Согласен" : "Agree", callback_data: `${language}_consent_agree` }],
+        [{ text: language === "rus" ? "Не согласен" : "Disagree", callback_data: `${language}_consent_disagree` }],
+      ],
+    }),
+  };
+  bot.sendMessage(chatId, texts[language].consentRequest, consentOptions);
+};
+
+const startRegistration = async (chatId) => {
+  const userLang = userResponses[chatId].language;
+  userResponses[chatId] = { language: userLang }; 
+  await bot.sendMessage(chatId, texts[userLang].askName);
+};
+
+bot.on("message", async (msg) => {
+  const chatId = msg.chat.id;
+  const userData = userResponses[chatId];
+  if (userData && userData.language) {
+    const userLang = userData.language;
+    if (!userData.name) {
+      userData.name = msg.text;
+      await bot.sendMessage(chatId, texts[userLang].askAge);
+    } else if (!userData.age) {
+      userData.age = msg.text;
+      await bot.sendMessage(chatId, texts[userLang].askCity);
+    } else if (!userData.city) {
+      userData.city = msg.text;
+      await bot.sendMessage(chatId, texts[userLang].askResume);
+    } else if (!userData.resume) {
+      userData.resume = msg.text;
+      await bot.sendMessage(chatId, texts[userLang].askExperience);
+    } else if (!userData.experience) {
+      userData.experience = msg.text;
+      await bot.sendMessage(chatId, texts[userLang].thanks);
+
+      saveToGoogleSheets(userData);
+      delete userResponses[chatId]; 
+    }
+  }
+});
+
+const saveToGoogleSheets = (userData) => {
+  console.log("Сохранение данных в Google Sheets:", userData);
+};
