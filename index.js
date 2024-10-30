f40fc6a5cf19ca20f9cc90062dbcd11e048c4cb6
aisuruq <aisuruqta@gmail.com>
2024-10-30 13:29:20 +0200

    Работает

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
+require('dotenv').config()
 const tgApi = require(`node-telegram-bot-api`);
 
-const bot = new TelegramBot(process.env.API_KEY_BOT, {
+const bot = new tgApi(process.env.API_KEY_BOT, {
     polling: true
 });
 
