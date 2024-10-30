1462571e6b61042ae17398bbab2191d6db89238c
Artem Kovalenko <105753291+ismellbitch@users.noreply.github.com>
2024-10-30 12:55:56 +0200

    init

--- index.js
+++ index.js
@@ -4,6 +4,13 @@ const token = "7733056715:AAGOCoyBTNjG2qeP6fQxMQsIYhyRL1EmJn8";
 
 const bot = new tgApi(token, { polling: true });
 
-bot.on("message", (msg) => {
-  console.log(msg);
+bot.setMyCommands([{ command: "/start", description: "Запуск бота" }]);
+
+bot.on("message", async (msg) => {
+  const text = msg.text;
+  const chatid = msg.chat.id;
+
+  if (text === "/start") {
+    await bot.sendMessage(chatid, "Приветствую!");
+  }
 });
