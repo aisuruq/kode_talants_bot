f7cc8704b15ad706b4db873374bee881bffa7937
Artem Kovalenko <105753291+ismellbitch@users.noreply.github.com>
2024-10-30 15:17:20 +0200

    added

--- index.js
+++ index.js
@@ -1,9 +1,34 @@
+require("dotenv").config();
 const tgApi = require(`node-telegram-bot-api`);
 
-const bot = new TelegramBot(process.env.API_KEY_BOT, {
-    polling: true
+const bot = new tgApi(process.env.API_KEY_BOT, {
+  polling: true,
 });
 
-bot.on("message", (msg) => {
+bot.setMyCommands([{ command: "/start", description: "Запуск бота" }]);
+
+const langOptions = {
+  reply_markup: JSON.stringify({
+    inline_keyboard: [
+      [{ text: `Русский`, callback_data: `rus` }],
+      [{ text: `English`, callback_data: `eng` }],
+    ],
+  }),
+};
+
+bot.on("message", async (msg) => {
+  const text = msg.text;
+  const chatid = msg.chat.id;
+
+  if (text === "/start") {
+    await bot.sendMessage(
+      chatid,
+      "Приветствую! Для начала выбери язык \n\n Hello! Choose a language",
+      langOptions
+    );
+  }
+});
+
+bot.on("callback_query", (msg) => {
   console.log(msg);
 });
