268fff575dac6dbd15ca84cb845ca86b6b787c3e
Artem Kovalenko <105753291+ismellbitch@users.noreply.github.com>
2024-10-31 19:27:15 +0200

    text added

--- text.js
+++ text.js
@@ -25,6 +25,17 @@ const texts = {
     askResume: "Отправьте ваше резюме в виде ссылки или файла",
     askExperience: "Опишите ваш коммерческий опыт",
     thanks: "Спасибо за заполнение информации!",
+    askTestRequest: "Желаете пройти проф. ориентационный тест?",
+    askQuestionOne:
+      "1. Какой из вариантов вам ближе?\n- А) Создание программного обеспечения\n- Б) Работа с данными и аналитика\n- В) Дизайн интерфейсов и пользовательского опыта\n- Г) Управление проектами и командами",
+    askQuestionTwo:
+      "2. Какая деятельность вам приносит наибольшее удовольствие?\n- А) Решение логических задач\n- Б) Исследование данных\n- В) Творческое оформление идей\n- Г) Организация процессов",
+    askQuestionThree:
+      "3. Как вы предпочитаете работать?\n- А) В одиночку\n- Б) В команде\n- В) В сотрудничестве с клиентами\n- Г) В управлении группой",
+    askQuestionFour:
+      "4. На какие навыки вы хотите сделать акцент?\n- А) Программирование\n- Б) Анализ данных\n- В) Дизайн\n- Г) Лидерство и управление",
+    askQuestionFive:
+      "5. Как вы справляетесь с новыми вызовами?\n- А) Ищу новые решения\n- Б) Анализирую и планирую\n- В) Оцениваю визуально\n- Г) Организую команду для обсуждения",
   },
   eng: {
     welcome: "You have selected English. Welcome to KODE talents!",
@@ -51,6 +62,17 @@ const texts = {
     askResume: "Send your resume by link or file",
     askExperience: "Describe your commercial experience",
     thanks: "Thanks for filling out the information!",
+    askTestRequest: "Would you like to take a career guidance test?",
+    askQuestionOne:
+      "1. Which option is closer to you?\n- A) Software development\t-B) Data management and analytics\t- C) Interface design and user experience\t- D) Project and team management",
+    askQuestionTwo:
+      "2. Which activity brings you the most pleasure?\n- A) Solving logical problems\t- B) Data research\t- C) Creative design of an idea\t- D) Organization of processes",
+    askQuestionThree:
+      "3. How do you prefer to work?\n- A) Alone\t- B) In a team\t- C) In cooperation with clients\t- D) In the management of the group",
+    askQuestionFour:
+      "4. What skills do you want to focus on?\n- A) Programming\t- B) Data Analysis\t- C) Design\t- D) Leadership and Management",
+    askQuestionFive:
+      "5. How do you cope with new challenges?\n-A) Looking for new solutions \t- B) Analyzing and planning \t- C) Evaluating visually\t- D) Organizing a team for discussion",
   },
 };
 
